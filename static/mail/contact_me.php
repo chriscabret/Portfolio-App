def2916a4703a63a6fa08377806e025f74a670f9
chriscabret <34635823+chriscabret@users.noreply.github.com>
2018-01-13 22:40:18 -0500

    Update contact_me.php

--- static/mail/contact_me.php
+++ static/mail/contact_me.php
@@ -59,7 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           $message_body .=  "$key: $value\n";
       }
       
-      $to = 'vladi@clevertechie.com';
+      $to = 'cabretchris@gmail.com';
       $subject = 'Contact Form Submit';
       if (mail($to, $subject, $message)){
           $success = "Message sent, thank you for contacting us!";
